90d5b8e67200875aee6090491528c36922380ece
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-05-25 20:29:05 -0500

    print version

--- src/app/features/home/contact/media/Media.react.js
+++ src/app/features/home/contact/media/Media.react.js
@@ -6,16 +6,21 @@ import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
+import {version} from '../../../../../../package.json';
 
 export default class Media extends React.Component {
   static propTypes = {};
+
+  printVersion = () => {
+    alert(`V ${version}`); // eslint-disable-line no-alert
+  };
 
   render() {
     return (
       <div id="media">
         <Card raised className="card-media">
           <CardContent>
-            <Avatar src={image} className="avatar"/>
+            <div onDoubleClick={this.printVersion}><Avatar src={image} className="avatar"/></div>
             <Grid container className="social-links" justify="space-around">
               <Fab><i className="fab fa-twitter" aria-hidden="true"/></Fab>
               <Fab><i className="fab fa-facebook" aria-hidden="true"/></Fab>
