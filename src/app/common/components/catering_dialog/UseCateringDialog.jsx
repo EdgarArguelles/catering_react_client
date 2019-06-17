dbe3c4539521c09735a412c699b03b6eaeadc33a
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-06-17 18:10:35 -0500

    migrate to hooks

--- src/app/common/components/catering_dialog/UseCateringDialog.jsx
+++ src/app/common/components/catering_dialog/UseCateringDialog.jsx
@@ -1,10 +1,11 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import {useDispatch} from 'react-redux';
 import NavigationActions from 'app/features/quotations/header/navigation/NavigationActions';
 
 export const useCateringDialog = (open, onClose) => {
   const dispatch = useDispatch();
+  const latestOnClose = useRef(onClose); // avoid to re-run useEffect when onClose change
   useEffect(() => {
-    setTimeout(() => dispatch(NavigationActions.closeNavigationDialog(open ? onClose : null)), 500);
-  }, [open, onClose, dispatch]);
+    setTimeout(() => dispatch(NavigationActions.closeNavigationDialog(open ? latestOnClose.current : null)), 500);
+  }, [open, dispatch]);
 };
