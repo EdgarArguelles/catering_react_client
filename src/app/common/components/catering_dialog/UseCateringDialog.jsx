bccd5e44ff49dc6be1933cefaf0d3c88cffc7818
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-06-18 12:12:50 -0500

    migrate

--- src/app/common/components/catering_dialog/UseCateringDialog.jsx
+++ src/app/common/components/catering_dialog/UseCateringDialog.jsx
@@ -5,7 +5,18 @@ import NavigationActions from 'app/features/quotations/header/navigation/Navigat
 export const useCateringDialog = (open, onClose) => {
   const dispatch = useDispatch();
   const latestOnClose = useRef(onClose); // avoid to re-run useEffect when onClose change
+  const latestOpen = useRef(open);
   useEffect(() => {
-    dispatch(NavigationActions.closeNavigationDialog(open ? latestOnClose.current : null));
+    if (open) {
+      dispatch(NavigationActions.closeNavigationDialog(latestOnClose.current));
+    } else if (latestOpen.current) {
+      // only close when last open value was true
+      dispatch(NavigationActions.closeNavigationDialog(null));
+    }
   }, [open, dispatch]);
+
+  // save last open value
+  useEffect(() => {
+    latestOpen.current = open;
+  }, [open]);
 };
