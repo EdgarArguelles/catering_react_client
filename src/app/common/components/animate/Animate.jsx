2a960281a0dc7b954ab8ce424afd6b6370b8380f
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-06-20 16:42:36 -0500

    migrate to hooks

--- src/app/common/components/animate/Animate.jsx
+++ src/app/common/components/animate/Animate.jsx
@@ -1,24 +1,24 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 
 const Animate = ({visible, className, animationIn, animationOut, delayOut, children}) => {
-  let timeout = null;
+  const timeout = useRef(null); // don't initialize timeout to null each render
   const [removedFromDOM, setRemovedFromDOM] = useState(false);
   const animation = visible ? animationIn : animationOut;
   const classes = [];
   animation && classes.push('animated', animation);
   className && classes.push(className);
   useEffect(() => {
-    return () => clearTimeout(timeout);
-  }, [timeout]);
+    return () => clearTimeout(timeout.current);
+  }, []);
 
   const toggleRemove = (delay = 0) => {
     const time = visible ? 0 : delay;
 
     // when component is visible, toggleRemove should move removedFromDOM to false after 0 delay
     // when component isn't visible, toggleRemove should move removedFromDOM to true after a delay
-    clearTimeout(timeout);
-    timeout = setTimeout(() => setRemovedFromDOM(!visible), time);
+    clearTimeout(timeout.current);
+    timeout.current = setTimeout(() => setRemovedFromDOM(!visible), time);
   };
 
   // don't call toggleRemove if isn't needed, toggleRemove moves removedFromDOM to false only when visible is true
