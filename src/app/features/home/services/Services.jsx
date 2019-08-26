6a351520345ae9421a95f6d2d9eb791407da31f2
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-08-26 17:46:10 -0500

    migrate to SVG icons

--- src/app/features/home/services/Services.jsx
+++ src/app/features/home/services/Services.jsx
@@ -1,5 +1,6 @@
 import './Services.scss';
 import React from 'react';
+import {faAt, faChartPie, faDesktop, faPaperPlane, faRoad, faShoppingBag} from '@fortawesome/free-solid-svg-icons';
 import Grid from '@material-ui/core/Grid';
 import SectionHeader from 'app/features/home/SectionHeader';
 import Service from './service/Service';
@@ -7,27 +8,27 @@ import Service from './service/Service';
 const Services = () => {
   const getServices = () => {
     return [
-      <Service title="Lorem Ipsum" iconClass="fas fa-desktop">
+      <Service title="Lorem Ipsum" icon={faDesktop}>
         Voluptatum deleniti atque corrupti quos dolores et quas molestias
         excepturi sint occaecati cupiditate non provident
       </Service>,
-      <Service title="Dolor Sitema" iconClass="fas fa-chart-pie">
+      <Service title="Dolor Sitemas" icon={faChartPie}>
         Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
         ex ea commodo consequat tarad limino ata
       </Service>,
-      <Service title="Sed ut perspiciatis" iconClass="fas fa-paper-plane">
+      <Service title="Sed ut perspiciatis" icon={faPaperPlane}>
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
         dolore eu fugiat nulla pariatur
       </Service>,
-      <Service title="Magni Dolores" iconClass="fas fa-at">
+      <Service title="Magni Dolor" icon={faAt}>
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
         deserunt mollit anim id est laborum
       </Service>,
-      <Service title="Nemo Enim" iconClass="fas fa-road">
+      <Service title="Nemo Enim" icon={faRoad}>
         At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
         praesentium voluptatum deleniti atque
       </Service>,
-      <Service title="Eiusmod Tempor" iconClass="fas fa-shopping-bag">
+      <Service title="Eiusmod Temp" icon={faShoppingBag}>
         Et harum quidem rerum facilis est et expedita distinctio. Nam libero
         tempore, cum soluta nobis est eligendi
       </Service>,
--- src/app/features/home/services/service/Service.jsx
+++ src/app/features/home/services/service/Service.jsx
@@ -2,13 +2,15 @@ import './Service.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {useSelector} from 'react-redux';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import Utils from 'app/common/Utils';
 
-const Service = ({title, iconClass, children}) => {
+const Service = ({title, icon, children}) => {
   const theme = useSelector(state => state.theme);
 
   return (
-    <div className={`${theme} service`}>
-      <div className="icon"><i className={iconClass} aria-hidden="true"/></div>
+    <div className={`${theme} service`} onMouseEnter={() => Utils.animateIcon(`${title}-icon`)}>
+      <div className="icon"><FontAwesomeIcon id={`${title}-icon`} icon={icon}/></div>
       <h4 className="title">{title}</h4>
       <p className="description">{children}</p>
     </div>
@@ -17,7 +19,7 @@ const Service = ({title, iconClass, children}) => {
 
 Service.propTypes = {
   title: PropTypes.string.isRequired,
-  iconClass: PropTypes.string.isRequired,
+  icon: PropTypes.object.isRequired,
   children: PropTypes.string.isRequired,
 };
 
