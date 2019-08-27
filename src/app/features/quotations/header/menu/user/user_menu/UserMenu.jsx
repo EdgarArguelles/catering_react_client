1512554e12df653000975e196294454e1a1723db
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-08-26 19:45:14 -0500

    migrate to SVG icons

--- src/app/features/quotations/header/menu/user/user_menu/UserMenu.jsx
+++ src/app/features/quotations/header/menu/user/user_menu/UserMenu.jsx
@@ -2,9 +2,12 @@ import './UserMenu.scss';
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {useDispatch} from 'react-redux';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faDonate, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
 import Popover from '@material-ui/core/Popover';
 import MenuItem from '@material-ui/core/MenuItem';
 import History from 'app/router/History';
+import Utils from 'app/common/Utils';
 import {useAppTheme} from 'app/common/Hooks';
 import ConfirmationDialog from 'app/common/components/confirmation_dialog/ConfirmationDialog';
 import AuthActions from 'app/features/auth/AuthActions';
@@ -13,10 +16,24 @@ const UserMenu = ({open, anchorEl, onClose}) => {
   const dispatch = useDispatch();
   const {theme, themeIcon, changeTheme} = useAppTheme();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const animateMyQuotationsIcon = () => Utils.animateIcon('menu-my-quotations-icon');
+  const animateThemeIcon = () => Utils.animateIcon('menu-theme-icon');
+  const animateSignOutIcon = () => Utils.animateIcon('menu-sign-out-icon');
 
   const redirectToMyQuotations = () => {
+    animateMyQuotationsIcon();
     onClose();
     History.navigate('/presupuestos/todos');
+  };
+
+  const handleChangeTheme = () => {
+    animateThemeIcon();
+    changeTheme();
+  };
+
+  const handleLogout = () => {
+    animateSignOutIcon();
+    setIsDialogOpen(true);
   };
 
   const logout = () => {
@@ -28,14 +45,15 @@ const UserMenu = ({open, anchorEl, onClose}) => {
   return (
     <Popover id="user-menu" open={open} anchorEl={anchorEl} onClose={onClose}
              anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}>
-      <MenuItem onClick={redirectToMyQuotations}>
-        <i className="fas fa-donate menu-icon" aria-hidden="true"/> Mis Presupuestos
+      <MenuItem onClick={redirectToMyQuotations} onMouseEnter={animateMyQuotationsIcon}>
+        <FontAwesomeIcon id="menu-my-quotations-icon" className="menu-icon" icon={faDonate}/> Mis Presupuestos
       </MenuItem>
-      <MenuItem onClick={changeTheme}>
-        <i className={`${themeIcon} menu-icon`} aria-hidden="true"/> Tema {theme === 'dark' ? 'Claro' : 'Oscuro'}
+      <MenuItem onClick={handleChangeTheme} onMouseEnter={animateThemeIcon}>
+        <FontAwesomeIcon id="menu-theme-icon" className="menu-icon" icon={themeIcon}/>
+        Tema {theme === 'dark' ? 'Claro' : 'Oscuro'}
       </MenuItem>
-      <MenuItem onClick={() => setIsDialogOpen(true)}>
-        <i className="fas fa-sign-out-alt menu-icon" aria-hidden="true"/> Salir
+      <MenuItem onClick={handleLogout} onMouseEnter={animateSignOutIcon}>
+        <FontAwesomeIcon id="menu-sign-out-icon" className="menu-icon" icon={faSignOutAlt}/> Salir
       </MenuItem>
 
       <ConfirmationDialog title="Salir" label="Al salir se perderan todos los cambios no guardados ¿Desea salir?"
