f72b05700bcf5c5e344b8b2de4315f65848fdfe5
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-09-24 12:34:15 -0500

    add whatsapp to Contact

--- src/app/features/home/contact/content/Content.jsx
+++ src/app/features/home/contact/content/Content.jsx
@@ -1,6 +1,7 @@
 import './Content.scss';
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faWhatsapp} from '@fortawesome/free-brands-svg-icons';
 import {faAt, faPhone} from '@fortawesome/free-solid-svg-icons';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,8 +9,9 @@ import Button from '@material-ui/core/Button';
 import Utils from 'app/common/Utils';
 
 const Content = () => {
-  const animateEmail = () => Utils.animateIcon('email-icon');
+  const animateWhatsapp = () => Utils.animateIcon('whatsapp-icon', {strokeWidth: 10});
   const animatePhone = () => Utils.animateIcon('phone-icon');
+  const animateEmail = () => Utils.animateIcon('email-icon');
 
   return (
     <div id="content">
@@ -23,10 +25,10 @@ const Content = () => {
             </p>
           </div>
           <div>
-            <Button className="email-button" onClick={animateEmail} onMouseEnter={animateEmail}>
-              <a href="mailto:caoc1688@gmail.com" tabIndex="-1">
-                <FontAwesomeIcon id="email-icon" icon={faAt}/>
-                <p className="button-label">caoc1688@gmail.com</p>
+            <Button onClick={animateWhatsapp} onMouseEnter={animateWhatsapp}>
+              <a href="https://wa.me/+524621073827" target="_blank" tabIndex="-1">
+                <FontAwesomeIcon id="whatsapp-icon" icon={faWhatsapp}/>
+                <p className="button-label">Whatsapp</p>
               </a>
             </Button>
           </div>
@@ -34,7 +36,15 @@ const Content = () => {
             <Button onClick={animatePhone} onMouseEnter={animatePhone}>
               <a href="tel:+524621073827" tabIndex="-1">
                 <FontAwesomeIcon id="phone-icon" icon={faPhone}/>
-                <p className="button-label">01 462 107 3827</p>
+                <p className="button-label">462 107 3827</p>
+              </a>
+            </Button>
+          </div>
+          <div>
+            <Button className="email-button" onClick={animateEmail} onMouseEnter={animateEmail}>
+              <a href="mailto:caoc1688@gmail.com" tabIndex="-1">
+                <FontAwesomeIcon id="email-icon" icon={faAt}/>
+                <p className="button-label">caoc1688@gmail.com</p>
               </a>
             </Button>
           </div>
