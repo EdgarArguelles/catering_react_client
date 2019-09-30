480c58f938c15a31167afc1ed43b14518947a86a
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-09-29 23:12:33 -0500

    create PostSlider Component

--- src/app/features/home/social/social_posts/SocialPosts.jsx
+++ src/app/features/home/social/social_posts/SocialPosts.jsx
@@ -1,73 +1,19 @@
 import './SocialPosts.scss';
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {EmbeddedPost} from 'react-facebook';
-import Slider from 'react-slick';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faChevronLeft, faChevronRight, faPause, faPlay, faTrophy, faUtensils} from '@fortawesome/free-solid-svg-icons';
+import {faTrophy, faUtensils} from '@fortawesome/free-solid-svg-icons';
 import Zoom from '@material-ui/core/Zoom';
-import IconButton from '@material-ui/core/IconButton';
 import FetchButton from 'app/common/components/fetch_button/FetchButton';
+import PostSlider from './post_slider/PostSlider';
 
 const SocialPosts = ({type}) => {
   const [showSlider, setShowSlider] = useState(false);
-  const [paused, setPaused] = useState(false);
   const isReview = type === 'review';
   const icon = isReview ? faTrophy : faUtensils;
   const label = isReview
     ? 'Revisa lo que nuestra comunidad piensa de nuestros servicios.'
     : 'Observa ejemplos de nuestros servicios más recientes.';
-  const photo1 = 'https://www.facebook.com/cansigno.de.la.torre/posts/673138676487438';
-  const photo2 = 'https://www.facebook.com/cansigno.de.la.torre/posts/665773830557256';
-  const photo3 = 'https://www.facebook.com/cansigno.de.la.torre/posts/664455697355736';
-  const review1 = 'https://www.facebook.com/cansigno.de.la.torre/posts/10219500860331624';
-  const review2 = 'https://www.facebook.com/cansigno.de.la.torre/posts/2381837775409677';
-  const review3 = 'https://www.facebook.com/cansigno.de.la.torre/posts/10156532633444117';
-
-  let slider;
-  const getSlider = () => {
-    const togglePause = () => {
-      paused && slider.slickPlay();
-      !paused && slider.slickPause();
-      setPaused(!paused);
-    };
-
-    const settings = {
-      lazyLoad: true,
-      centerMode: true,
-      variableWidth: true,
-      dots: true,
-      autoplay: true,
-      arrows: false,
-      responsive: [
-        {breakpoint: 1920, settings: {centerMode: true, variableWidth: true}},
-        {breakpoint: 390, settings: {centerMode: false, variableWidth: false}},
-      ],
-    };
-
-    return (
-      <div className="post-slider">
-        <div className="slider-controls">
-          <IconButton onClick={() => slider.slickPrev()}><FontAwesomeIcon icon={faChevronLeft}/></IconButton>
-          <IconButton onClick={togglePause}><FontAwesomeIcon icon={paused ? faPlay : faPause}/></IconButton>
-          <IconButton onClick={() => slider.slickNext()}><FontAwesomeIcon icon={faChevronRight}/></IconButton>
-        </div>
-
-        <Slider className="post-carousel" ref={element => (slider = element)} {...settings}>
-          <EmbeddedPost href={isReview ? review1 : photo1} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review2 : photo2} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review3 : photo3} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review1 : photo1} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review2 : photo2} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review3 : photo3} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review1 : photo1} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review2 : photo2} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review3 : photo3} showText={isReview}/>
-          <EmbeddedPost href={isReview ? review1 : photo1} showText={isReview}/>
-        </Slider>
-      </div>
-    );
-  };
 
   return (
     <div className="social-posts">
@@ -77,7 +23,7 @@ const SocialPosts = ({type}) => {
       <FetchButton id={`${type}-button`} color="primary" label="Cargar desde Facebook" hidden={showSlider} icon={icon}
                    asyncCall={async () => await new Promise(resolve => setTimeout(resolve, 3000))}
                    onComplete={() => setShowSlider(true)}/>
-      {showSlider && getSlider()}
+      {showSlider && <PostSlider type={type}/>}
     </div>
   );
 };
