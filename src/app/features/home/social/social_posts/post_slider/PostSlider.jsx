e1508ded44ba3870145608aae13d197709596216
edgararguelles <edgar.arguelles@harvardbusiness.org>
2019-10-04 12:47:49 -0500

    load post dynamically

--- src/app/features/home/social/social_posts/post_slider/PostSlider.jsx
+++ src/app/features/home/social/social_posts/post_slider/PostSlider.jsx
@@ -1,15 +1,21 @@
 import './PostSlider.scss';
 import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
+import {useDispatch, useSelector} from 'react-redux';
 import {EmbeddedPost} from 'react-facebook';
 import Slider from 'react-slick';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faChevronLeft, faChevronRight, faPause, faPlay} from '@fortawesome/free-solid-svg-icons';
 import IconButton from '@material-ui/core/IconButton';
+import AppActions from 'app/AppActions';
 
 const PostSlider = ({type}) => {
+  const dispatch = useDispatch();
+  const token = useSelector(state => state.app.facebookAccessCode);
   const [slider, setSlider] = useState(null);
   const [paused, setPaused] = useState(false);
+  const [waiting, setWaiting] = useState(false);
+  const [posts, setPosts] = useState([]);
   const isReview = type === 'review';
   const photo1 = 'https://www.facebook.com/cansigno.de.la.torre/posts/673138676487438';
   const photo2 = 'https://www.facebook.com/cansigno.de.la.torre/posts/665773830557256';
@@ -17,6 +23,33 @@ const PostSlider = ({type}) => {
   const review1 = 'https://www.facebook.com/cansigno.de.la.torre/posts/10219500860331624';
   const review2 = 'https://www.facebook.com/cansigno.de.la.torre/posts/2381837775409677';
   const review3 = 'https://www.facebook.com/cansigno.de.la.torre/posts/10156532633444117';
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      if (!token) {
+        dispatch(AppActions.getFacebookAccessCode());
+        return;
+      }
+
+      setWaiting(true);
+      const endpoint = isReview ? 'ratings?fields=recommendation_type%2Copen_graph_story' : 'photos?type=uploaded';
+      const url = `https://graph.facebook.com/v4.0/615026265632013/${endpoint}&limit=10&access_token=${token}`;
+      const response = await fetch(url);
+      const payload = await response.json();
+
+      if (payload.error) {
+        await dispatch(AppActions.getFacebookAccessCode());
+        setWaiting(false);
+      } else {
+        const array = isReview ?
+          payload.data.filter(p => p.recommendation_type === 'positive').map(p => p.open_graph_story.id) :
+          payload.data.map(post => post.id);
+        setPosts(array);
+      }
+    };
+
+    !waiting && loadPosts();
+  }, [dispatch, waiting, isReview, token]);
 
   useEffect(() => {
     const fixSlider = () => {
