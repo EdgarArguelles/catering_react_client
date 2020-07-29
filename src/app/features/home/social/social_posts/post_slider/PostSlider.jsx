6938daca547d794a3dfaf2b739b7278195239115
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-07-29 13:16:27 -0500

    update libraries

--- src/app/features/home/social/social_posts/post_slider/PostSlider.jsx
+++ src/app/features/home/social/social_posts/post_slider/PostSlider.jsx
@@ -1,6 +1,7 @@
 import './PostSlider.scss';
 import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
+import {useSelector} from 'react-redux';
 import {EmbeddedPost} from 'react-facebook';
 import Slider from 'react-slick';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
@@ -8,6 +9,7 @@ import {faChevronLeft, faChevronRight, faPause, faPlay} from '@fortawesome/free-
 import IconButton from '@material-ui/core/IconButton';
 
 const PostSlider = ({posts, showText}) => {
+  const theme = useSelector(state => state.app.theme);
   const [slider, setSlider] = useState(null);
   const [paused, setPaused] = useState(false);
   const url = 'https://www.facebook.com/cansigno.de.la.torre/posts';
@@ -35,8 +37,8 @@ const PostSlider = ({posts, showText}) => {
         <IconButton onClick={togglePause}><FontAwesomeIcon icon={paused ? faPlay : faPause}/></IconButton>
         <IconButton onClick={() => slider.slickNext()}><FontAwesomeIcon icon={faChevronRight}/></IconButton>
       </div>
-      <Slider className="post-carousel" ref={element => setSlider(element)} lazyLoad={false} centerMode={true}
-              variableWidth={true} adaptiveHeight={true} arrows={false} dots={true} autoplay={true}>
+      <Slider className={`${theme} post-carousel`} ref={element => setSlider(element)} lazyLoad={false}
+              centerMode={true} variableWidth={true} adaptiveHeight={true} arrows={false} dots={true} autoplay={true}>
         {posts.map(post => <EmbeddedPost key={post} href={`${url}/${post}`} showText={showText}/>)}
       </Slider>
     </div>
