339833d484c9177455e7f5f6e51663f84b0ab32b
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-08-01 16:38:00 -0500

    migrate

--- src/app/features/quotations/quotation/QuotationReducer.js
+++ src/app/features/quotations/quotation/QuotationReducer.js
@@ -1,70 +1,71 @@
-/**
- * Given the same arguments, it should calculate the next state and return it.
- * No surprises. No side effects. No API calls. No mutations. Just a calculation.
- */
-import {ACTION_TYPES as DATA_ACTION_TYPES} from 'app/data/quotations/QuotationsActions';
-import {ACTION_TYPES} from './QuotationActions';
-import menuReducer from 'app/features/quotations/menu/MenuReducer';
+import {createSlice} from '@reduxjs/toolkit';
+import {fetchQuotation} from 'app/data/quotations/QuotationsReducer';
+import menuReducer, {cleanData} from 'app/features/quotations/menu/MenuReducer';
 
-const name = (state = '', action) => {
-  switch (action.type) {
-    case ACTION_TYPES.QUOTATION_CHANGE_NAME:
-      return action.payload.name;
-    default:
-      return state;
-  }
+const SLICE_NAME = 'QUOTATION';
+
+const overwriteLocal = quotation => {
+  return {
+    ...quotation,
+    menus: quotation.menus.map(menu => ({
+      ...menu,
+      price: menu.courses.reduce(
+        (accumulator, course) => accumulator + course.dishes.reduce((sum, dish) => sum + dish.price, 0),
+        0,
+      ),
+    })),
+  };
 };
 
-const menus = (state = [], action) => {
-  switch (action.type) {
-    case ACTION_TYPES.QUOTATION_ADD_NEW_MENU:
-      return [...state, menuReducer({id: action.payload.menuId}, action)];
-    case ACTION_TYPES.QUOTATION_ADD_MENU:
-      return [...state, action.payload.menu];
-    case ACTION_TYPES.QUOTATION_REMOVE_MENU:
-      return state.filter(menu => menu.id !== action.payload.menuId);
-    case ACTION_TYPES.QUOTATION_SELECT_MENU:
-      return state.map(menu => ({...menu, isSelected: menu.id === action.payload.menuId}));
-    default:
-      const selected = state.find(menu => menu.isSelected);
-      if (!selected) {
+const quotationSlice = createSlice({
+  name: SLICE_NAME,
+  initialState: {
+    name: '',
+    menus: [],
+    price: 0,
+  },
+  reducers: {
+    changeName(state, {payload: name}) {
+      state.name = name;
+    },
+    selectMenu(state, {payload: menuId}) {
+      state.menus.forEach(menu => (menu.isSelected = menu.id === menuId));
+    },
+    addNewMenu(state, {payload: menuId}) {
+      state.menus.push(menuReducer({id: menuId}, {type: cleanData.type}));
+    },
+    addMenu(state, {payload: menu}) {
+      state.menus.push(menu);
+    },
+    removeMenu(state, {payload: menuId}) {
+      state.menus = state.menus.filter(menu => menu.id !== menuId);
+    },
+    setPrice(state, {payload: price}) {
+      state.price = price;
+    },
+    revertQuotation(state, {payload: quotation}) {
+      return overwriteLocal(quotation);
+    },
+  },
+  extraReducers: builder =>
+    builder
+      .addCase(fetchQuotation.fulfilled, (state, action) => {
+        if (action.payload.overwriteLocalChanges) {
+          return overwriteLocal(action.payload.data);
+        }
+
         return state;
-      }
+      })
+      .addDefaultCase((state, action) => {
+        const selected = state.menus.find(menu => menu.isSelected);
+        if (selected) {
+          const noSelected = state.menus.filter(menu => !menu.isSelected);
+          state.menus = [...noSelected, menuReducer(selected, action)];
+        }
+      }),
+});
 
-      const noSelected = state.filter(menu => !menu.isSelected);
-      return [...noSelected, menuReducer(selected, action)];
-  }
-};
-
-const price = (state = 0, action) => {
-  switch (action.type) {
-    case ACTION_TYPES.QUOTATION_SET_PRICE:
-      return action.payload.amount;
-    default:
-      return state;
-  }
-};
-
-export default (state = {}, action = {}) => {
-  switch (action.type) {
-    case DATA_ACTION_TYPES.QUOTATION_OVERWRITE_LOCAL:
-    case ACTION_TYPES.QUOTATION_REVERT:
-      return {
-        ...action.payload.data,
-        menus: action.payload.data.menus.map(menu => ({
-          ...menu,
-          price: menu.courses.reduce(
-            (accumulator, course) => accumulator + course.dishes.reduce((sum, dish) => sum + dish.price, 0),
-            0,
-          ),
-        })),
-      };
-    default:
-      return {
-        ...state,
-        name: name(state.name, action),
-        menus: menus(state.menus, action),
-        price: price(state.price, action),
-      };
-  }
-};
+export default quotationSlice.reducer;
+export const {
+  changeName, selectMenu, addNewMenu, addMenu, removeMenu, setPrice, revertQuotation,
+} = quotationSlice.actions;
