d61d8794dafa6db6c714cd4d5def8c32f153b774
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-08-02 00:58:31 -0500

    migrate

--- test/app/features/quotations/auth_dialog/AuthDialogReducer.spec.js
+++ test/app/features/quotations/auth_dialog/AuthDialogReducer.spec.js
@@ -1,34 +1,50 @@
-import {ACTION_TYPES} from 'app/features/quotations/auth_dialog/AuthDialogActions';
-import {isAuthDialogOpen} from 'app/features/quotations/auth_dialog/AuthDialogReducer';
+import AuthDialogReducer, {
+  closeAuthDialog,
+  openAuthDialog,
+} from 'app/features/quotations/auth_dialog/AuthDialogReducer';
 
-describe('Quotations -> Auth Dialog -> Reducer', () => {
-  it('should get default state when empty', () => {
-    const state = false;
+describe('Quotations -> Auth Dialog -> Reducer/Actions', () => {
+  describe('Reducer', () => {
+    it('should get default state when undefined', () => {
+      const state = false;
 
-    const result = isAuthDialogOpen();
+      const result = AuthDialogReducer(undefined, {type: 'invalid'});
 
-    expect(result).toStrictEqual(state);
-  });
+      expect(result).toStrictEqual(state);
+    });
 
-  it('should get the same original status when action is not allow', () => {
-    const state = true;
+    it('should get the same original status when action is not allow', () => {
+      const state = true;
 
-    const result = isAuthDialogOpen(state, {type: 'invalid'});
+      const result = AuthDialogReducer(state, {type: 'invalid'});
 
-    expect(result).toStrictEqual(state);
-    // don't mutate
-    expect(state).toStrictEqual(true);
-  });
+      expect(result).toStrictEqual(state);
+      // don't mutate
+      expect(state).toStrictEqual(true);
+    });
 
-  it('should take isAuthDialogOpen value from payload when action is CHANGE_IS_AUTH_DIALOG_OPEN', () => {
-    const state = true;
-    const stateExpected = false;
-    const action = {type: ACTION_TYPES.CHANGE_IS_AUTH_DIALOG_OPEN, payload: {isAuthDialogOpen: false}};
+    it('should change isAuthDialogOpen to true when action is openAuthDialog', () => {
+      const state = false;
+      const stateExpected = true;
+      const action = {type: openAuthDialog.type};
 
-    const result = isAuthDialogOpen(state, action);
+      const result = AuthDialogReducer(state, action);
 
-    expect(result).toStrictEqual(stateExpected);
-    // don't mutate
-    expect(state).toStrictEqual(true);
+      expect(result).toStrictEqual(stateExpected);
+      // don't mutate
+      expect(state).toStrictEqual(false);
+    });
+
+    it('should change isAuthDialogOpen to false when action is closeAuthDialog', () => {
+      const state = true;
+      const stateExpected = false;
+      const action = {type: closeAuthDialog.type};
+
+      const result = AuthDialogReducer(state, action);
+
+      expect(result).toStrictEqual(stateExpected);
+      // don't mutate
+      expect(state).toStrictEqual(true);
+    });
   });
 });
