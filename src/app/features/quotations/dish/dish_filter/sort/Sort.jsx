fa8cd1ce0c0ca2a3bd548dd7be602d755a6cfd55
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-08-02 17:15:57 -0500

    migrate

--- src/app/features/quotations/dish/dish_filter/sort/Sort.jsx
+++ src/app/features/quotations/dish/dish_filter/sort/Sort.jsx
@@ -6,20 +6,20 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import Utils from 'app/common/Utils';
-import DishFilterActions from 'app/features/quotations/dish/dish_filter/DishFilterActions';
+import {changeSort} from 'app/features/quotations/dish/dish_filter/DishFilterReducer';
 
 const Sort = ({value, label, icon}) => {
   const dispatch = useDispatch();
   const filter = useSelector(state => state.quotations.dish.filter);
   const className = filter.sort === value ? 'active' : '';
-  const changeSort = () => {
+  const moveSort = () => {
     Utils.animateIcon(`${label}-sort-icon`);
-    dispatch(DishFilterActions.changeSort(value));
+    dispatch(changeSort(value));
   };
 
   return (
     <Grid item xs={6} sm={4} className="filter-sort">
-      <Fab className={className} onClick={changeSort}><FontAwesomeIcon id={`${label}-sort-icon`} icon={icon}/></Fab>
+      <Fab className={className} onClick={moveSort}><FontAwesomeIcon id={`${label}-sort-icon`} icon={icon}/></Fab>
       <p>{label}</p>
     </Grid>
   );
