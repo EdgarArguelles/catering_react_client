7ccf440ad9661c03f50a7bfe41378594e2feafc0
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-08-02 01:08:02 -0500

    migrate

--- test/app/features/quotations/header/navigation/NavigationReducer.spec.js
+++ test/app/features/quotations/header/navigation/NavigationReducer.spec.js
@@ -1,40 +1,42 @@
 /* eslint-disable max-lines */
-import {ACTION_TYPES} from 'app/features/quotations/header/navigation/NavigationActions';
-import NavigationReducer from 'app/features/quotations/header/navigation/NavigationReducer';
+import NavigationReducer, {
+  changeNavigation,
+  closeNavigationDialog,
+} from 'app/features/quotations/header/navigation/NavigationReducer';
 
-describe('Quotations -> Header -> Navigation -> Reducer', () => {
-  it('should get default state when empty', () => {
-    const state = {
-      backLink: '',
-      title: '',
-      closeDialog: null,
-    };
+describe('Quotations -> Header -> Navigation -> Reducer/Actions', () => {
+  describe('Reducer', () => {
+    it('should get default state when undefined', () => {
+      const state = {
+        backLink: '',
+        title: '',
+        closeDialog: null,
+      };
 
-    const result = NavigationReducer();
+      const result = NavigationReducer(undefined, {type: 'invalid'});
 
-    expect(result).toStrictEqual(state);
-  });
-
-  it('should get the same original status when action is not allow', () => {
-    const state = {
-      backLink: 'abc',
-      title: '123',
-      closeDialog: {id: 'ID1'},
-    };
-
-    const result = NavigationReducer(state, {type: 'invalid'});
-
-    expect(result).toStrictEqual(state);
-    // don't mutate
-    expect(state).toStrictEqual({
-      backLink: 'abc',
-      title: '123',
-      closeDialog: {id: 'ID1'},
+      expect(result).toStrictEqual(state);
     });
-  });
 
-  describe('backLink', () => {
-    it('should change backLink value when action is CHANGE_NAVIGATION', () => {
+    it('should get the same original status when action is not allow', () => {
+      const state = {
+        backLink: 'abc',
+        title: '123',
+        closeDialog: {id: 'ID1'},
+      };
+
+      const result = NavigationReducer(state, {type: 'invalid'});
+
+      expect(result).toStrictEqual(state);
+      // don't mutate
+      expect(state).toStrictEqual({
+        backLink: 'abc',
+        title: '123',
+        closeDialog: {id: 'ID1'},
+      });
+    });
+
+    it('should change backLink value when action is changeNavigation', () => {
       const state = {
         backLink: 'abc',
         title: '123',
@@ -42,10 +44,10 @@ describe('Quotations -> Header -> Navigation -> Reducer', () => {
       };
       const stateExpected = {
         backLink: 'abc123',
-        title: undefined,
+        title: '',
         closeDialog: null,
       };
-      const action = {type: ACTION_TYPES.CHANGE_NAVIGATION, payload: {backLink: 'abc123'}};
+      const action = {type: changeNavigation.type, payload: {backLink: 'abc123'}};
 
       const result = NavigationReducer(state, action);
 
@@ -57,10 +59,8 @@ describe('Quotations -> Header -> Navigation -> Reducer', () => {
         closeDialog: {id: 'ID1'},
       });
     });
-  });
 
-  describe('title', () => {
-    it('should change title value when action is CHANGE_NAVIGATION', () => {
+    it('should change title value when action is changeNavigation', () => {
       const state = {
         backLink: 'abc',
         title: '123',
@@ -71,33 +71,7 @@ describe('Quotations -> Header -> Navigation -> Reducer', () => {
         title: '123abc',
         closeDialog: null,
       };
-      const action = {type: ACTION_TYPES.CHANGE_NAVIGATION, payload: {title: '123abc'}};
-
-      const result = NavigationReducer(state, action);
-
-      expect(result).toStrictEqual(stateExpected);
-      // don't mutate
-      expect(state).toStrictEqual({
-        backLink: 'abc',
-        title: '123',
-        closeDialog: {id: 'ID1'},
-      });
-    });
-  });
-
-  describe('closeDialog', () => {
-    it('should change closeDialog value when action is CLOSE_NAVIGATION_DIALOG', () => {
-      const state = {
-        backLink: 'abc',
-        title: '123',
-        closeDialog: {id: 'ID1'},
-      };
-      const stateExpected = {
-        backLink: 'abc',
-        title: '123',
-        closeDialog: {id: 'ID22'},
-      };
-      const action = {type: ACTION_TYPES.CLOSE_NAVIGATION_DIALOG, payload: {closeDialog: {id: 'ID22'}}};
+      const action = {type: changeNavigation.type, payload: {title: '123abc'}};
 
       const result = NavigationReducer(state, action);
 
@@ -110,7 +84,7 @@ describe('Quotations -> Header -> Navigation -> Reducer', () => {
       });
     });
 
-    it('should set to null when action is CHANGE_NAVIGATION', () => {
+    it('should set closeDialog to null when action is changeNavigation', () => {
       const state = {
         backLink: 'abc',
         title: '123',
@@ -121,7 +95,31 @@ describe('Quotations -> Header -> Navigation -> Reducer', () => {
         title: 'b',
         closeDialog: null,
       };
-      const action = {type: ACTION_TYPES.CHANGE_NAVIGATION, payload: {backLink: 'a', title: 'b'}};
+      const action = {type: changeNavigation.type, payload: {backLink: 'a', title: 'b'}};
+
+      const result = NavigationReducer(state, action);
+
+      expect(result).toStrictEqual(stateExpected);
+      // don't mutate
+      expect(state).toStrictEqual({
+        backLink: 'abc',
+        title: '123',
+        closeDialog: {id: 'ID1'},
+      });
+    });
+
+    it('should change closeDialog value when action is closeNavigationDialog', () => {
+      const state = {
+        backLink: 'abc',
+        title: '123',
+        closeDialog: {id: 'ID1'},
+      };
+      const stateExpected = {
+        backLink: 'abc',
+        title: '123',
+        closeDialog: {id: 'ID22'},
+      };
+      const action = {type: closeNavigationDialog.type, payload: {id: 'ID22'}};
 
       const result = NavigationReducer(state, action);
 
