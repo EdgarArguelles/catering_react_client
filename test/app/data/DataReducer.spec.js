d2090bc156f2c4e12f6487cc3ce31c147dccc799
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-08-03 12:02:41 -0500

    migrate

--- test/app/data/DataReducer.spec.js
+++ test/app/data/DataReducer.spec.js
@@ -1,125 +1,96 @@
 /* eslint-disable max-lines */
-import {ACTION_TYPES} from 'app/data/DataActions';
-import Data from 'app/data/DataReducer';
+import dataReducer, {changeVersion} from 'app/data/DataReducer';
 
-describe('Data -> Reducer', () => {
-  it('should get default state when undefined', () => {
-    const state = {
-      version: null,
-      fetching: {
-        courseTypes: false,
-        dishes: false,
-        dish: {},
-      },
-      courseTypes: null,
-      dishes: null,
-      quotations: {
-        fetching: false,
-        error: null,
-        data: null,
-        metaData: null,
-      },
-    };
+describe('Data -> Reducer/Actions', () => {
+  describe('Reducer', () => {
+    it('should get default state when undefined', () => {
+      const state = {
+        version: null,
+        courseTypes: {data: null},
+        dishes: {data: null, fetching: {}},
+        quotations: {
+          fetching: false,
+          error: null,
+          data: null,
+          metaData: null,
+        },
+      };
 
-    const result = Data(undefined, {type: 'invalid'});
+      const result = dataReducer(undefined, {type: 'invalid'});
 
-    expect(result).toStrictEqual(state);
-  });
-
-  it('should get the same original status when action is not allow', () => {
-    const state = {
-      version: 'abc',
-      fetching: {
-        courseTypes: true,
-        dishes: true,
-        dish: {a: true, b: false},
-      },
-      courseTypes: {id: 'ID1'},
-      dishes: {id: 'ID2'},
-      quotations: {
-        fetching: true,
-        error: {id: 'error'},
-        data: {id: 'ID3'},
-        metaData: {id: 'meta'},
-      },
-    };
-
-    const result = Data(state, {type: 'invalid'});
-
-    expect(result).toStrictEqual(state);
-    // don't mutate
-    expect(state).toStrictEqual({
-      version: 'abc',
-      fetching: {
-        courseTypes: true,
-        dishes: true,
-        dish: {a: true, b: false},
-      },
-      courseTypes: {id: 'ID1'},
-      dishes: {id: 'ID2'},
-      quotations: {
-        fetching: true,
-        error: {id: 'error'},
-        data: {id: 'ID3'},
-        metaData: {id: 'meta'},
-      },
+      expect(result).toStrictEqual(state);
     });
-  });
 
-  it('should get version when action is DATA_CHANGE_VERSION', () => {
-    const state = {
-      version: 'abc',
-      fetching: {
-        courseTypes: true,
-        dishes: true,
-        dish: {a: true, b: false},
-      },
-      courseTypes: {id: 'ID1'},
-      dishes: {id: 'ID2'},
-      quotations: {
-        fetching: true,
-        error: {id: 'error'},
-        data: {id: 'ID3'},
-        metaData: {id: 'meta'},
-      },
-    };
-    const stateExpected = {
-      version: '123',
-      fetching: {
-        courseTypes: true,
-        dishes: true,
-        dish: {a: true, b: false},
-      },
-      courseTypes: {id: 'ID1'},
-      dishes: null,
-      quotations: {
-        fetching: true,
-        error: {id: 'error'},
-        data: {id: 'ID3'},
-        metaData: {id: 'meta'},
-      },
-    };
-    const action = {type: ACTION_TYPES.DATA_CHANGE_VERSION, payload: {version: '123'}};
+    it('should get the same original status when action is not allow', () => {
+      const state = {
+        version: 'abc',
+        courseTypes: {data: {id: 'ID1'}},
+        dishes: {data: {id: 'ID2'}, fetching: {a: true, b: false}},
+        quotations: {
+          fetching: true,
+          error: {id: 'error'},
+          data: {id: 'ID3'},
+          metaData: {id: 'meta'},
+        },
+      };
 
-    const result = Data(state, action);
+      const result = dataReducer(state, {type: 'invalid'});
 
-    expect(result).toStrictEqual(stateExpected);
-    // don't mutate
-    expect(state).toStrictEqual({
-      version: 'abc',
-      fetching: {
-        courseTypes: true,
-        dishes: true,
-        dish: {a: true, b: false},
-      },
-      courseTypes: {id: 'ID1'},
-      dishes: {id: 'ID2'},
-      quotations: {
-        fetching: true,
-        error: {id: 'error'},
-        data: {id: 'ID3'},
-        metaData: {id: 'meta'},
-      },
+      expect(result).toStrictEqual(state);
+      // don't mutate
+      expect(state).toStrictEqual({
+        version: 'abc',
+        courseTypes: {data: {id: 'ID1'}},
+        dishes: {data: {id: 'ID2'}, fetching: {a: true, b: false}},
+        quotations: {
+          fetching: true,
+          error: {id: 'error'},
+          data: {id: 'ID3'},
+          metaData: {id: 'meta'},
+        },
+      });
+    });
+
+    it('should get version when action is changeVersion', () => {
+      const state = {
+        version: 'abc',
+        courseTypes: {data: {id: 'ID1'}},
+        dishes: {data: {id: 'ID2'}, fetching: {a: true, b: false}},
+        quotations: {
+          fetching: true,
+          error: {id: 'error'},
+          data: {id: 'ID3'},
+          metaData: {id: 'meta'},
+        },
+      };
+      const stateExpected = {
+        version: '123',
+        courseTypes: {data: {id: 'ID1'}},
+        dishes: {data: null, fetching: {a: true, b: false}},
+        quotations: {
+          fetching: true,
+          error: {id: 'error'},
+          data: {id: 'ID3'},
+          metaData: {id: 'meta'},
+        },
+      };
+      const action = {type: changeVersion.type, payload: '123'};
+
+      const result = dataReducer(state, action);
+
+      expect(result).toStrictEqual(stateExpected);
+      // don't mutate
+      expect(state).toStrictEqual({
+        version: 'abc',
+        courseTypes: {data: {id: 'ID1'}},
+        dishes: {data: {id: 'ID2'}, fetching: {a: true, b: false}},
+        quotations: {
+          fetching: true,
+          error: {id: 'error'},
+          data: {id: 'ID3'},
+          metaData: {id: 'meta'},
+        },
+      });
     });
   });
 });
