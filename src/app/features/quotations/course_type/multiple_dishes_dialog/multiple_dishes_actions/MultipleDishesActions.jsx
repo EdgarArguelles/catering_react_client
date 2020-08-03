75534df59c0ed01867794c94b530f8e38f1f0763
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-08-02 19:36:54 -0500

    migrate

--- src/app/features/quotations/course_type/multiple_dishes_dialog/multiple_dishes_actions/MultipleDishesActions.jsx
+++ src/app/features/quotations/course_type/multiple_dishes_dialog/multiple_dishes_actions/MultipleDishesActions.jsx
@@ -6,15 +6,14 @@ import Button from '@material-ui/core/Button';
 import {useAreDishesLoaded} from 'app/common/Hooks';
 import {getDishesPrice} from 'app/features/quotations/dish/Dish.service';
 import {addCourse, increasePrice} from 'app/features/quotations/menu/MenuReducer';
-import MultipleDishesDialogActions
-  from 'app/features/quotations/course_type/multiple_dishes_dialog/MultipleDishesDialogActions';
+import {cleanDishes} from 'app/features/quotations/course_type/multiple_dishes_dialog/MultipleDishesDialogReducer';
 
 const MultipleDishesActions = ({courseType, onClose}) => {
   const dispatch = useDispatch();
   const allDishes = useSelector(state => state.data.dishes);
   const menuCourses = useSelector(state => state.quotations.quotation.menus.find(menu => menu.isSelected).courses);
   const multipleDishes = useSelector(state => state.quotations.multipleDishesDialog.dishes);
-  const cleanDishes = () => dispatch(MultipleDishesDialogActions.cleanDishes());
+  const handleCleanDishes = () => dispatch(cleanDishes());
   const handleAddCourse = (dishesIds, position) => dispatch(addCourse({
     courseTypeId: courseType.id,
     dishesIds,
@@ -28,7 +27,7 @@ const MultipleDishesActions = ({courseType, onClose}) => {
     const coursePrice = getDishesPrice(multipleDishes, allDishes);
     handleAddCourse(multipleDishes.map(dish => dish.id), courseTypeCourses.length + 1);
     handleIncreasePrice(coursePrice);
-    cleanDishes();
+    handleCleanDishes();
     onClose();
   };
 
