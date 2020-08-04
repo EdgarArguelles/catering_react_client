da78c565a9ef112aa296aaaa6ff856ac06877c23
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-08-04 16:27:19 -0500

    migrate

--- src/app/data/course_types/CourseTypesReducer.js
+++ src/app/data/course_types/CourseTypesReducer.js
@@ -1,29 +1,41 @@
-/**
- * Given the same arguments, it should calculate the next state and return it.
- * No surprises. No side effects. No API calls. No mutations. Just a calculation.
- */
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import Api from 'app/common/Api';
 import Utils from 'app/common/Utils';
-import {ACTION_TYPES} from './CourseTypesActions';
 
-export const courseTypesFetching = (state = false, action = {}) => {
-  switch (action.type) {
-    case ACTION_TYPES.FETCH_COURSE_TYPES_REQUEST:
-      return true;
-    case ACTION_TYPES.FETCH_COURSE_TYPES_SUCCESS:
-    case ACTION_TYPES.FETCH_COURSE_TYPES_ERROR:
-      return false;
-    default:
-      return state;
-  }
-};
+const SLICE_NAME = 'DATA_COURSE_TYPES';
 
-export const courseTypes = (state = null, action = {}) => {
-  switch (action.type) {
-    case ACTION_TYPES.SET_COURSE_TYPES:
-      return action.payload.courseTypes;
-    case ACTION_TYPES.FETCH_COURSE_TYPES_SUCCESS:
-      return Utils.arrayToObject(action.payload.data);
-    default:
-      return state;
-  }
-};
+export const fetchCourseTypes = createAsyncThunk(
+  `${SLICE_NAME}/fetchCourseTypes`,
+  async (arg, thunkAPI) => {
+    const body = {query: '{activeCourseTypes {id name picture position status}}'};
+    const versionBody = {query: '{version {version}}'};
+
+    const versionJson = await Api.graphql(thunkAPI.dispatch, versionBody);
+    const json = await Api.graphql(thunkAPI.dispatch, body);
+    return {
+      data: json.data.activeCourseTypes,
+      metaData: {version: versionJson.data.version.version},
+    };
+  },
+);
+
+const courseTypesDataSlice = createSlice({
+  name: SLICE_NAME,
+  initialState: {
+    data: null,
+  },
+  reducers: {
+    setCourseTypesData(state, {payload: courseTypesData}) {
+      state.data = courseTypesData;
+    },
+  },
+  extraReducers: builder => {
+    return builder
+      .addCase(fetchCourseTypes.fulfilled, (state, action) => {
+        state.data = Utils.arrayToObject(action.payload.data);
+      });
+  },
+});
+
+export default courseTypesDataSlice.reducer;
+export const {setCourseTypesData} = courseTypesDataSlice.actions;
