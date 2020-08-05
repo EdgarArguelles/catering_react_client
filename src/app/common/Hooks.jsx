19bec90b02341b76384b5b26116405e5a71cf854
edgararguelles <ing.edgar.arguelles@gmail.com>
2020-08-04 21:50:06 -0500

    migrate

--- src/app/common/Hooks.jsx
+++ src/app/common/Hooks.jsx
@@ -7,7 +7,7 @@ import {fetchCompleteQuotation} from 'app/features/quotations/quotation/Quotatio
 import {changeTheme} from 'app/AppReducer';
 import {closeNavigationDialog} from 'app/features/quotations/header/navigation/NavigationReducer';
 import {fetchQuotation} from 'app/data/quotations/QuotationsReducer';
-import DishesActions from 'app/data/dishes/DishesActions';
+import {fetchDish} from 'app/data/dishes/DishesReducer';
 import {fetchPing} from 'app/features/auth/AuthReducer';
 
 export const useAppTheme = () => {
@@ -67,17 +67,17 @@ export const useQuotationsLoader = () => {
 
 export const useAreDishesLoaded = dishes => {
   const dispatch = useDispatch();
-  const dishFetching = useSelector(state => state.data.fetching.dish);
-  const allDishes = useSelector(state => state.data.dishes);
+  const dishFetching = useSelector(state => state.data.dishes.fetching);
+  const allDishes = useSelector(state => state.data.dishes.data);
 
   const latestDishes = useRef(dishes); // avoid to re-run useEffect when dishes changes
   const latestDishFetching = useRef(dishFetching); // avoid to re-run useEffect when dishFetching changes
   const latestAllDishes = useRef(allDishes); // avoid to re-run useEffect when allDishes changes
 
   useEffect(() => {
-    const fetchDish = dishId => dispatch(DishesActions.fetchDish(dishId));
+    const handleFetchDish = dishId => dispatch(fetchDish(dishId));
 
-    fetchDishesList(latestDishes.current, latestAllDishes.current, latestDishFetching.current, fetchDish);
+    fetchDishesList(latestDishes.current, latestAllDishes.current, latestDishFetching.current, handleFetchDish);
   }, [dispatch]);
 
   return areAllDishesPresent(dishes, allDishes) || dishes.length === 0;
