36342727cfd4ef68370497470f4683967f184574
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-01-25 12:50:22 -0600

    create TestHelper

---
+++ test/TestHelper.jsx
@@ -0,0 +1,34 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
+import {createStore} from 'redux';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import reducers from 'app/Reducers';
+
+const getStore = (dispatchStub, state, useRealStore) => {
+  const store = configureStore()(state);
+  store.dispatch = dispatchStub;
+  return useRealStore ? createStore(reducers, state) : store;
+};
+
+export const renderQueryComponent = (Component, {dispatchStub, setQueryDataStub}) => (hook, state, useRealStore) => {
+  const queryClient = new QueryClient();
+  setQueryDataStub && (queryClient.setQueryData = setQueryDataStub);
+
+  const store = getStore(dispatchStub, state, useRealStore);
+  const wrapper = renderer.create(
+    <Provider store={store}><QueryClientProvider client={queryClient}>
+      <Component hook={hook}/>
+    </QueryClientProvider></Provider>,
+  );
+
+  return {wrapper, store};
+};
+
+export const renderReduxComponent = (Component, dispatchStub) => (hook, state, useRealStore) => {
+  const store = getStore(dispatchStub, state, useRealStore);
+  const wrapper = renderer.create(<Provider store={store}><Component hook={hook}/></Provider>);
+
+  return {wrapper, store};
+};
