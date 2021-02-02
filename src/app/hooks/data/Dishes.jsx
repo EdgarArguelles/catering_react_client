ade55b7b5dbbd011cf7b992c17bc9e164d6dd433
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-02-01 19:30:33 -0600

    create 'react-query'

--- src/app/hooks/data/Dishes.jsx
+++ src/app/hooks/data/Dishes.jsx
@@ -81,5 +81,5 @@ export const useDishesByIds = (dishesId = []) => {
   }));
   const dishes = results.filter(result => result.data).map(result => result.data);
   const isAnyFetching = !!results.filter(result => result.isFetching).length;
-  return {dishes, isAnyFetching};
+  return {...results, dishes, isAnyFetching};
 };
