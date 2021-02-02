f83125fd57caa69ceb814033c21c47bca6bfd368
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-02-01 20:06:19 -0600

    improve Quotations.jsx

--- src/app/hooks/data/Quotations.jsx
+++ src/app/hooks/data/Quotations.jsx
@@ -1,19 +1,27 @@
 import {useDispatch} from 'react-redux';
-import {useQuery} from 'react-query';
+import {useInfiniteQuery} from 'react-query';
 import Api from 'app/common/Api';
 import Utils from 'app/common/Utils';
 
-export const useQuotations = (pagination, loggedUser) => {
+export const useQuotations = loggedUser => {
   const KEY = 'Quotations';
   const dispatch = useDispatch();
-  return useQuery(KEY, async () => {
+  const DEFAULT_PAGE_PARAM = {page: 0, size: 5, sort: ['createdAt'], direction: 'DESC'};
+  const results = useInfiniteQuery(KEY, async ({pageParam = DEFAULT_PAGE_PARAM}) => {
     const FIELDS = 'totalElements totalPages content{id name createdAt price}';
-    const body = {query: `{quotationPage(pageDataRequest: ${Utils.stringifyPageDataRequest(pagination)}) {${FIELDS}}}`};
+    const body = {query: `{quotationPage(pageDataRequest: ${Utils.stringifyPageDataRequest(pageParam)}) {${FIELDS}}}`};
     const json = await Api.graphql(dispatch, body);
     const {content, totalElements, totalPages} = json.data.quotationPage;
     return {
       quotations: content,
-      metaData: {pagination: {...pagination}, totalElements, totalPages},
+      metaData: {pagination: {...pageParam}, totalElements, totalPages},
     };
-  }, {enabled: !!pagination && !!loggedUser});
+  }, {
+    enabled: !!loggedUser,
+    getNextPageParam: ({metaData: {pagination, totalPages}}) =>
+      pagination.page + 1 < totalPages ? {...pagination, page: pagination.page + 1} : undefined,
+  });
+  const quotations = results.data?.pages.map((page => page.quotations)).flat();
+  const metaData = results.data?.pages.map((page => page.metaData)).pop();
+  return {...results, quotations, metaData};
 };
