c7d1bad90cb92af8df967f02f38441a15576b4a6
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-02-05 11:35:42 -0600

    migrate QuotationToolbar.jsx

--- src/app/features/quotations/quotation/quotation_list/quotation_toolbar/QuotationToolbar.jsx
+++ src/app/features/quotations/quotation/quotation_list/quotation_toolbar/QuotationToolbar.jsx
@@ -1,6 +1,6 @@
 import './QuotationToolbar.scss';
 import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
+import {useQueryClient} from 'react-query';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faChevronDown} from '@fortawesome/free-solid-svg-icons';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -9,12 +9,11 @@ import Popover from '@material-ui/core/Popover';
 import MenuItem from '@material-ui/core/MenuItem';
 import Radio from '@material-ui/core/Radio';
 import Utils from 'app/common/Utils';
-import {useQuotations} from 'app/hooks/data/Quotations';
+import {QUOTATIONS_KEY, useQuotations} from 'app/hooks/data/Quotations';
 import ExpandBar from 'app/common/components/expand_bar/ExpandBar';
-import {cleanQuotations, fetchQuotations} from 'app/data/quotations/QuotationsReducer';
 
 const QuotationToolbar = () => {
-  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
   const {metaData} = useQuotations();
   const [isCompact, setIsCompact] = useState(false);
   const [open, setOpen] = useState(false);
@@ -32,13 +31,12 @@ const QuotationToolbar = () => {
 
   const handleChangeSort = newSort => {
     if (metaData.pagination.sort[0] !== newSort) {
-      dispatch(cleanQuotations());
-      dispatch(fetchQuotations({
+      Utils.resetInfiniteQuery(queryClient, QUOTATIONS_KEY, {
         ...metaData.pagination,
         sort: [newSort, 'createdAt'],
         direction: newSort === 'createdAt' ? 'DESC' : 'ASC',
         page: 0,
-      }));
+      });
     }
 
     handleClose();
