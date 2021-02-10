8693315aa11f6189a2d7f254e19eabcde7f3dae8
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-02-10 11:32:40 -0600

    create use Create and Edit Quotation

--- src/app/hooks/data/Quotations.jsx
+++ src/app/hooks/data/Quotations.jsx
@@ -1,10 +1,27 @@
 import {useDispatch, useSelector} from 'react-redux';
-import {useInfiniteQuery, useQuery} from 'react-query';
+import {useInfiniteQuery, useMutation, useQuery} from 'react-query';
 import Api from 'app/common/Api';
 import Utils from 'app/common/Utils';
 
 export const QUOTATION_KEY = 'Quotation';
 export const QUOTATIONS_KEY = 'Quotations';
+
+/**
+ * Clean extra information from quotation (not needed in graphql mutation)
+ *
+ * @param {Object} quotation data to be cleaned
+ * @return {String} data cleaned
+ */
+const getQuotationFixed = quotation => {
+  const quotationFixed = {...quotation, menus: []};
+  quotation.menus && quotation.menus.forEach(menu => quotationFixed.menus.push({...menu}));
+  quotationFixed.menus.forEach(menu => {
+    delete menu.price;
+    delete menu.isSelected;
+  });
+
+  return Utils.stringifyObjectWithNoQuotesOnKeys(quotationFixed);
+};
 
 export const useQuotation = quotationId => {
   const KEY = [QUOTATION_KEY, quotationId];
@@ -39,4 +56,26 @@ export const useQuotations = () => {
   const quotations = results.data?.pages.map((page => page.quotations)).flat();
   const metaData = results.data?.pages.map((page => page.metaData)).pop();
   return {...results, quotations, metaData};
+};
+
+export const useCreateQuotation = () => {
+  const dispatch = useDispatch();
+  const loggedUser = useSelector(state => state.auth.loggedUser);
+  return useMutation(async quotation => {
+    const body = {query: `mutation {createQuotation(quotation: ${getQuotationFixed(quotation)}) {id}}`};
+    // create a fake delay (ignore it in test cases)
+    process.env.NODE_ENV !== 'test' && await new Promise(resolve => setTimeout(resolve, 3000));
+    return await Api.graphql(dispatch, body);
+  }, {enabled: !!loggedUser});
+};
+
+export const useEditQuotation = () => {
+  const dispatch = useDispatch();
+  const loggedUser = useSelector(state => state.auth.loggedUser);
+  return useMutation(async quotation => {
+    const body = {query: `mutation {updateQuotation(quotation: ${getQuotationFixed(quotation)}) {id}}`};
+    // create a fake delay (ignore it in test cases)
+    process.env.NODE_ENV !== 'test' && await new Promise(resolve => setTimeout(resolve, 3000));
+    return await Api.graphql(dispatch, body);
+  }, {enabled: !!loggedUser});
 };
