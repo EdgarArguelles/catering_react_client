0890b5bc81df40d62d338b79c3b51a980e74259b
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-02-11 23:56:25 -0600

    fix QuotationNotify.jsx

--- src/app/features/quotations/quotation/quotation_edit/quotation_edit_content/quotation_notify/QuotationNotify.jsx
+++ src/app/features/quotations/quotation/quotation_edit/quotation_edit_content/quotation_notify/QuotationNotify.jsx
@@ -12,13 +12,14 @@ import Animate from 'app/common/components/animate/Animate';
 import ConfirmationDialog from 'app/common/components/confirmation_dialog/ConfirmationDialog';
 
 const QuotationNotify = () => {
+  const isRemoteProcessing = useSelector(state => state.data.quotations.isRemoteProcessing);
   const quotation = useSelector(state => state.quotations.quotation);
   const {data: remote, isFetching} = useQuotation(quotation.id);
   const [isSendDialogOpen, setIsSendDialogOpen] = useState(false);
   const [comment, setComment] = useState('');
   const link = `${window.location.origin}/presupuestos/ver/${quotation.id}`;
   const body = `comentario:%0D%0A${encodeURI(comment)}%0D%0A%0D%0A%0D%0Alink: ${link}`;
-  const visible = !isFetching && areEqual(quotation, remote);
+  const visible = !isRemoteProcessing && !isFetching && areEqual(quotation, remote);
   const openSendDialog = () => {
     Utils.animateIcon('quotation-notify-button-icon', {strokeWidth: 20});
     setIsSendDialogOpen(true);
