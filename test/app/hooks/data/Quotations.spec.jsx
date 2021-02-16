b306ed2277b8cda7c0f95d15b747654997ed0818
edgararguelles <ing.edgar.arguelles@gmail.com>
2021-02-15 21:57:07 -0600

    complete Quotations.spec.jsx

--- test/app/hooks/data/Quotations.spec.jsx
+++ test/app/hooks/data/Quotations.spec.jsx
@@ -6,7 +6,15 @@ import React from 'react';
 import {renderQueryComponent} from 'app/../../test/TestHelper';
 import Api from 'app/common/Api';
 import Utils from 'app/common/Utils';
-import {useQuotation, useQuotations} from 'app/hooks/data/Quotations';
+import {
+  QUOTATIONS_KEY,
+  useCreateQuotation,
+  useDeleteQuotation,
+  useEditQuotation,
+  useQuotation,
+  useQuotations,
+} from 'app/hooks/data/Quotations';
+import {changeError, changeFetching} from 'app/data/quotations/QuotationsReducer';
 
 describe('Hooks -> Data -> Quotations', () => {
   const dispatchStub = sinon.stub();
@@ -155,6 +163,144 @@ describe('Hooks -> Data -> Quotations', () => {
       sinon.assert.callCount(graphqlStub, 2);
       sinon.assert.calledWithExactly(graphqlStub, dispatchStub, body);
       sinon.assert.calledWithExactly(graphqlStub, dispatchStub, body2);
+      sinon.assert.callCount(invalidateQueriesStub, 0);
+    });
+  });
+
+  describe('useCreateQuotation', () => {
+    const quotation = {
+      id: 8,
+      menus: [{id: 1, price: 123, isSelected: true}, {id: 2, price: 789, isSelected: false}],
+    };
+    const body = {query: 'mutation {createQuotation(quotation: {id:8,menus:[{id:1},{id:2}]}) {id}}'};
+
+    it('should create quotation successfully', async () => {
+      const jsonExpected = {data: {createQuotation: {id: 1}}};
+      graphqlStub.withArgs(dispatchStub, body).returns(jsonExpected);
+
+      mountComponent(() => useCreateQuotation(), {auth: {loggedUser: {}}}, false);
+
+      const result = await hookResponse.mutateAsync(quotation);
+      expect(result).toStrictEqual({id: 1});
+      sinon.assert.callCount(dispatchStub, 3);
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: true});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: null});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: false});
+      sinon.assert.callCount(graphqlStub, 1);
+      sinon.assert.calledWithExactly(graphqlStub, dispatchStub, body);
+      sinon.assert.callCount(invalidateQueriesStub, 1);
+      sinon.assert.calledWithExactly(invalidateQueriesStub, QUOTATIONS_KEY);
+    });
+
+    it('should handle create quotation error', async () => {
+      const errorExpected = new Error('error 1');
+      graphqlStub.withArgs(dispatchStub, body).throws(errorExpected);
+
+      mountComponent(() => useCreateQuotation(), {auth: {loggedUser: {}}}, false);
+
+      try {
+        await hookResponse.mutateAsync(quotation);
+        // eslint-disable-next-line no-empty
+      } catch (error) {
+      }
+      sinon.assert.callCount(dispatchStub, 4);
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: true});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: null});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: errorExpected});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: false});
+      sinon.assert.callCount(graphqlStub, 1);
+      sinon.assert.calledWithExactly(graphqlStub, dispatchStub, body);
+      sinon.assert.callCount(invalidateQueriesStub, 0);
+    });
+  });
+
+  describe('useEditQuotation', () => {
+    const quotation = {
+      id: 9,
+      menus: [{id: 1, price: 123, isSelected: true}, {id: 2, price: 789, isSelected: false}],
+    };
+    const body = {query: 'mutation {updateQuotation(quotation: {id:9,menus:[{id:1},{id:2}]}) {id}}'};
+
+    it('should create quotation successfully', async () => {
+      const jsonExpected = {data: {updateQuotation: {id: 1}}};
+      graphqlStub.withArgs(dispatchStub, body).returns(jsonExpected);
+
+      mountComponent(() => useEditQuotation(), {auth: {loggedUser: {}}}, false);
+
+      const result = await hookResponse.mutateAsync(quotation);
+      expect(result).toStrictEqual({id: 1});
+      sinon.assert.callCount(dispatchStub, 3);
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: true});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: null});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: false});
+      sinon.assert.callCount(graphqlStub, 1);
+      sinon.assert.calledWithExactly(graphqlStub, dispatchStub, body);
+      sinon.assert.callCount(invalidateQueriesStub, 1);
+      sinon.assert.calledWithExactly(invalidateQueriesStub, QUOTATIONS_KEY);
+    });
+
+    it('should handle create quotation error', async () => {
+      const errorExpected = new Error('error 1');
+      graphqlStub.withArgs(dispatchStub, body).throws(errorExpected);
+
+      mountComponent(() => useEditQuotation(), {auth: {loggedUser: {}}}, false);
+
+      try {
+        await hookResponse.mutateAsync(quotation);
+        // eslint-disable-next-line no-empty
+      } catch (error) {
+      }
+      sinon.assert.callCount(dispatchStub, 4);
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: true});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: null});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: errorExpected});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: false});
+      sinon.assert.callCount(graphqlStub, 1);
+      sinon.assert.calledWithExactly(graphqlStub, dispatchStub, body);
+      sinon.assert.callCount(invalidateQueriesStub, 0);
+    });
+  });
+
+  describe('useDeleteQuotation', () => {
+    const quotationId = 7;
+    const body = {query: 'mutation {deleteQuotation(id: 7) {id}}'};
+
+    it('should create quotation successfully', async () => {
+      const jsonExpected = {data: {deleteQuotation: {id: 1}}};
+      graphqlStub.withArgs(dispatchStub, body).returns(jsonExpected);
+
+      mountComponent(() => useDeleteQuotation(), {auth: {loggedUser: {}}}, false);
+
+      const result = await hookResponse.mutateAsync(quotationId);
+      expect(result).toStrictEqual({id: 1});
+      sinon.assert.callCount(dispatchStub, 3);
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: true});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: null});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: false});
+      sinon.assert.callCount(graphqlStub, 1);
+      sinon.assert.calledWithExactly(graphqlStub, dispatchStub, body);
+      sinon.assert.callCount(invalidateQueriesStub, 1);
+      sinon.assert.calledWithExactly(invalidateQueriesStub, QUOTATIONS_KEY);
+    });
+
+    it('should handle create quotation error', async () => {
+      const errorExpected = new Error('error 1');
+      graphqlStub.withArgs(dispatchStub, body).throws(errorExpected);
+
+      mountComponent(() => useDeleteQuotation(), {auth: {loggedUser: {}}}, false);
+
+      try {
+        await hookResponse.mutateAsync(quotationId);
+        // eslint-disable-next-line no-empty
+      } catch (error) {
+      }
+      sinon.assert.callCount(dispatchStub, 4);
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: true});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: null});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeError.type, payload: errorExpected});
+      sinon.assert.calledWithExactly(dispatchStub, {type: changeFetching.type, payload: false});
+      sinon.assert.callCount(graphqlStub, 1);
+      sinon.assert.calledWithExactly(graphqlStub, dispatchStub, body);
       sinon.assert.callCount(invalidateQueriesStub, 0);
     });
   });
