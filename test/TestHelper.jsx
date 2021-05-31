01149c6f7fb9feb1d0ee39291a3a51081a4cd309
Edgar Arguelles <ing.edgar.arguelles@gmail.com>
2021-05-30 23:09:21 -0500

    Refactor

--- test/TestHelper.jsx
+++ test/TestHelper.jsx
@@ -1,9 +1,9 @@
 import renderer from 'react-test-renderer';
 import React from 'react';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import {createStore} from 'redux';
-import {QueryClient, QueryClientProvider} from 'react-query';
+import { createStore } from 'redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import reducers from 'app/Reducers';
 
 const getStore = (dispatchStub, state, useRealStore) => {
@@ -19,7 +19,7 @@ export const renderQueryComponent = (Component, {
   removeQueriesStub,
 }) =>
   (hook, state, useRealStore) => {
-    const queryClient = new QueryClient({defaultOptions: {queries: {retry: false, staleTime: Infinity}}});
+    const queryClient = new QueryClient({ defaultOptions: { queries: { retry: false, staleTime: Infinity } } });
     setQueryDataStub && (queryClient.setQueryData = setQueryDataStub);
     invalidateQueriesStub && (queryClient.invalidateQueries = invalidateQueriesStub);
     removeQueriesStub && (queryClient.removeQueries = removeQueriesStub);
@@ -27,16 +27,16 @@ export const renderQueryComponent = (Component, {
     const store = getStore(dispatchStub, state, useRealStore);
     const wrapper = renderer.create(
       <Provider store={store}><QueryClientProvider client={queryClient}>
-        <Component hook={hook}/>
+        <Component hook={hook} />
       </QueryClientProvider></Provider>,
     );
 
-    return {wrapper, store};
+    return { wrapper, store };
   };
 
 export const renderReduxComponent = (Component, dispatchStub) => (hook, state, useRealStore) => {
   const store = getStore(dispatchStub, state, useRealStore);
-  const wrapper = renderer.create(<Provider store={store}><Component hook={hook}/></Provider>);
+  const wrapper = renderer.create(<Provider store={store}><Component hook={hook} /></Provider>);
 
-  return {wrapper, store};
+  return { wrapper, store };
 };
